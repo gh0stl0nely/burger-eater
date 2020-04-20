40aed10a87324ea95e7d013801b542a2e6734545
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2020-04-19 21:42:59 -0400

    Added details

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,20 @@
 const mysql = require('mysql')
 const dotenv = require('dotenv');
 dotenv.config();
+var connection;
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: process.env.PASSWORD,
-    database: 'burgers_db'
-})
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: process.env.PASSWORD,
+        database: 'burgers_db'
+    })
+}
+
 
 connection.connect((e) => {
     if(e) throw e
